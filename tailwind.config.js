805f3c8b5ede8e7b43c5d998f3c70bbf182c4f78
Praneeth860 <mpraneeth474@gmail.com>
2024-10-25 11:52:04 +0530

    added images and setup for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        my_primary: "hsl(178, 100%, 8%)",
+      },
+    },
   },
   plugins: [],
 };
