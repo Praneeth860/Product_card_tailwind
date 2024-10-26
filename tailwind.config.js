f2cbaff578ee0f1b8d4f35cbda54b9a110178e57
Praneeth860 <mpraneeth474@gmail.com>
2024-10-26 22:27:18 +0530

    created a card , had its content,added font families and added pretier plugin for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        my_primary: "hsl(178, 100%, 8%)",
+        pale_orange: "#ffd9a6",
+        light_orange: "#fbb03b",
+        orange: "#f7931e",
+      },
+      fontFamily: {
+        sans: ["Josefin Sans", "sans-serif"],
+        title: ["Playfair Display", "serif"],
       },
     },
   },
